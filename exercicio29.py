c887c16b7cf1bebcf51c6a1d6b53218e4fd29ed6
victorbonomi16 <bononikis@gmail.com>
2020-08-31 14:35:20 -0300

    feat: Exercicio 29 adicionado

---
+++ exercicio29.py
@@ -0,0 +1,9 @@
+n1 = int (input("informe o primeira nota  :" ))
+n2 = int (input("informe o segunda nota :" ))
+n3 = int (input("informe o terceira nota  :" ))
+n4 = int (input("informe o quarta nota  :" ))
+
+media=((n1+n2+n3+n4)/4)
+ 
+
+print(" media aritmetica : ",media)  
